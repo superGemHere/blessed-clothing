4af03fc80af2db8e025e97d92daca21518e81419
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-30 18:20:21 +0300

    modify startup files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/outlet/Header/Header.jsx'
+
+function App() {
+
+  return (
+     <Header />
+  )
+}
+
+export default App
