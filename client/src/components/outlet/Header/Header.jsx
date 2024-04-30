6d13a0183f3691c759e97b4917fe55eba3c697bf
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-30 19:18:30 +0300

    + make dynamic header
    TODO : dropdown

--- client/src/components/outlet/Header/Header.jsx
+++ client/src/components/outlet/Header/Header.jsx
@@ -1,11 +1,13 @@
 import styles from "./header.module.css";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header(){
     return(
         <header className={styles.header}>
             <div className={styles.left}>
-                <ul className={styles.nav__ul}>
+                <ul className={styles.nav__ul}  id={styles.left}>
                     <li className={styles.ul__item}>
                         <Link to="#" className={styles.ul__links}>Home</Link>
                     </li>
@@ -16,18 +18,20 @@ export default function Header(){
                         <Link to="#" className={styles.ul__links}>About Us</Link>
                     </li>
                 </ul>
+                {/* <MenuIcon id={styles.menu__button}/> */}
             </div>
             <h1 className={styles.logo}>Blessed</h1>
             <div className={styles.right}>
-                <ul className={styles.nav__ul}>
+                <ul className={styles.nav__ul} id={styles.right}>
                         <li className={styles.ul__item}>
                             <Link to="#" className={styles.ul__links}>Browse</Link>
                         </li>
                         <li className={styles.ul__item}>
-                            <Link to="#" className={styles.ul__links}>Cart</Link>
+                            <Link to="#" className={styles.ul__links}><ShoppingCartIcon  id={styles.cart__button}/></Link>
                         </li>
                 </ul>
             </div>
+                <MenuIcon id={styles.menu__button}/>
         </header>
     );
 }
