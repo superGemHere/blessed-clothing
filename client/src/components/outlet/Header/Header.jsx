1b207cdb1797ebf92ef35b78fb246428869e4873
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-05-01 20:01:30 +0300

    + responsive navbar

--- client/src/components/outlet/Header/Header.jsx
+++ client/src/components/outlet/Header/Header.jsx
@@ -2,23 +2,46 @@ import styles from "./header.module.css";
 import {Link} from "react-router-dom";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useEffect, useRef, useState } from "react";
 
 export default function Header(){
+    // const responsiveNav = useRef();
+    const [isVisible, setIsVis] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if(window.innerWidth > 990){
+                setIsVis(false);
+            }
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        // Cleanup function to remove event listener when component unmounts
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, [])
+
+
     return(
         <header className={styles.header}>
+            <nav>
             <div className={styles.left}>
                 <ul className={styles.nav__ul}  id={styles.left}>
                     <li className={styles.ul__item}>
-                        <Link to="#" className={styles.ul__links}>Home</Link>
+                        <Link to="/" className={styles.ul__links}>Home</Link>
                     </li>
                     <li className={styles.ul__item}>
                         <Link to="#" className={styles.ul__links}>Shop</Link>
                     </li>
                     <li className={styles.ul__item}>
+                        <Link to="#" className={styles.ul__links}>Sale</Link>
+                    </li>
+                    <li className={styles.ul__item}>
                         <Link to="#" className={styles.ul__links}>About Us</Link>
                     </li>
                 </ul>
-                {/* <MenuIcon id={styles.menu__button}/> */}
             </div>
             <h1 className={styles.logo}>Blessed</h1>
             <div className={styles.right}>
@@ -27,11 +50,44 @@ export default function Header(){
                             <Link to="#" className={styles.ul__links}>Browse</Link>
                         </li>
                         <li className={styles.ul__item}>
+                            <Link to="#" className={styles.ul__links}>Collections</Link>
+                        </li>
+                        <li className={styles.ul__item}>
                             <Link to="#" className={styles.ul__links}><ShoppingCartIcon  id={styles.cart__button}/></Link>
                         </li>
                 </ul>
             </div>
-                <MenuIcon id={styles.menu__button}/>
+                {isVisible 
+                ? <CloseIcon id={styles.menu__button} onClick={() => setIsVis(!isVisible)}/> 
+                : <MenuIcon id={styles.menu__button} onClick={() => setIsVis(!isVisible)}/>}
+            </nav>
+            {isVisible &&
+            <div className={styles.dropDown} /*ref={responsiveNav}*/>
+            <ul className={styles.responsiveNav}  >
+                    <li className={styles.ul__item}>
+                        <Link to="/" className={styles.ul__links}>Home</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                        <Link to="#" className={styles.ul__links}>Shop</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                        <Link to="#" className={styles.ul__links}>Sale</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                        <Link to="#" className={styles.ul__links}>About Us</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                            <Link to="#" className={styles.ul__links}>Browse</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                            <Link to="#" className={styles.ul__links}>Collections</Link>
+                    </li>
+                    <li className={styles.ul__item}>
+                        <Link to="#" className={styles.ul__links}><ShoppingCartIcon  id={styles.cart__button}/></Link>
+                    </li>
+                </ul>
+            </div>
+            }
         </header>
     );
 }
