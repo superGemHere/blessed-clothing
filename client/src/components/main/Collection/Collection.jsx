66eb52d4f65dbfba8be130eff782e270bef10b6c
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-05-13 18:52:00 +0300

    + hover collection card
    TODO: Fix the see collection btn glitchy renderin

--- client/src/components/main/Collection/Collection.jsx
+++ client/src/components/main/Collection/Collection.jsx
@@ -4,21 +4,22 @@ export default function Collection ({collectionData, image}){
                             
     return(
         <div className={styles.card}>
-            <h1>{collectionData.name}</h1>
+            
             {/* <div className={styles.image} style={{backgroundImage: `url(${image})`}}>
                 <p>{collectionData.description}</p>
             </div> */}
-            <div className={styles.imageContainer}>
+            <div className={styles.imageContainer} style={{backgroundImage: `url(${image})`}}>
                 <div className={styles.gradient}></div>
-                <img src={image} alt="collection image" className={styles.image}/>
-            </div>
-            <div className={styles.order}>
-                <div className={styles.priceDiv}>
-                </div>
-                <p className={styles.orderBtn}>
+                {/* <img src={image} alt="collection image" className={styles.image}/> */}
+                <h1 className={styles.collectionName}>{collectionData.name}</h1>
+                <p className={styles.seeCollectionBtn}>
                     See Collection
                 </p>
             </div>
+            {/* <div className={styles.order}>
+                <div className={styles.priceDiv}>
+                </div>
+            </div> */}
         </div>
     );
 }
