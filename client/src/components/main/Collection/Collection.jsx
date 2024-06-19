72573cfb9fe59bf15f420b1aeb2dab60c5d53160
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-19 19:36:58 +0300

    Update Collection.jsx

--- client/src/components/main/Collection/Collection.jsx
+++ client/src/components/main/Collection/Collection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import styles from './collection.module.css';
 
 const CtaComponent = ({
@@ -7,14 +8,15 @@ const CtaComponent = ({
 }) => {
   return (
     <>
-      <div className={styles.cta}>
-        <img
+      <div className={styles.cta} style={{backgroundImage: `url(${image})`}}>
+        {/* <img
           src={image}
           alt=""
-          />
+          /> */}
         <div className={styles.text}>
           <h2>{collectionData.name}</h2>
           <p>{collectionData.description}</p>
+          <Link className={styles.detailsLink} to={"#"}><span>See Collection</span></Link>
           {/* Here might go the redirect btn */}
         </div>
       </div>
