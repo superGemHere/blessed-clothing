05955bf0951c9d53a3c0909714bb214ad6192d95
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-04 19:09:15 +0300

    ADD footer jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/outlet/Header/Header.jsx'
 import Home from './components/main/Home/Home.jsx'
+import Footer from './components/outlet/Footer/Footer.jsx'
 
 function App() {
 
@@ -11,8 +12,8 @@ function App() {
     <Routes>
 
       <Route path="/" element={<Home />} />
-
     </Routes>
+    <Footer />
     </>
   )
 }
