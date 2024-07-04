2c8ef268f1ee55bd9b8f4bdec3e4b211d0fac436
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-04 21:10:01 +0300

    Update Footer.jsx

--- client/src/components/outlet/Footer/Footer.jsx
+++ client/src/components/outlet/Footer/Footer.jsx
@@ -1,10 +1,37 @@
-import styles from './footer.module.css'
+import styles from './footer.module.css';
+
+import {Link} from 'react-router-dom';
+
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer (){
-                            
+
     return(
         <div className={styles.container}>
-              				
+            <div className={styles.links}>
+                <section className={styles.navigation}>
+                    <Link className={styles.link} to={"/"}>Home</Link>
+                    <Link className={styles.link} to={"#"}>Shops</Link>
+                    <Link className={styles.link} to={"#"}>Sale</Link>
+                    <Link className={styles.link} to={"#"}>Browse</Link>
+                    <Link className={styles.link} to={"#"}>Collections</Link>
+                </section>
+                <section className={styles.about}>
+                    <Link className={styles.link} to={"#"}>About Us</Link>
+                    <Link className={styles.link} to={"#"}>FAQ</Link>
+                    <Link className={styles.link} to={"#"}>Locations</Link>
+                </section>	
+                <section className={styles.contacts}>
+                    <p className={styles.paragraph}>Phone:&nbsp;&nbsp;<span className={styles.phoneNumber}>+878 559 4478</span></p>
+                    <p className={styles.paragraph}>E-mail: blessedClothing@gmail.com</p>
+                    <p className={styles.paragraph}>Linked in: Blessed</p>
+                </section>
+            </div>
+            <div className={styles.github}>
+                <FaGithub color='#fff'size={35}/>
+                <Link className={styles.link} target="_blank" to={"https://github.com/supergemhere"}>superGemHere</Link>
+            </div>
+            <p className={styles.copyRight}>All rights reserved &copy; - Dzhem Syuleyman</p>
         </div>
     );
 }
