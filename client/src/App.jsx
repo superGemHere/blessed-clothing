21fec6e529c06a9437d7070c1b0eb6c9262a5503
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-06 19:25:43 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/outlet/Header/Header.jsx'
 import Home from './components/main/Home/Home.jsx'
 import Footer from './components/outlet/Footer/Footer.jsx'
+import Products from './components/main/Products/Products.jsx'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
 
       <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
     <Footer />
     </>
