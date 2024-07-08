994ad7f9146af7e9d935dcb5d51ff0d89190b2d6
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-08 22:04:38 +0300

    Create product card structure
    TODO: styling

---
+++ client/src/components/main/Product/Product.jsx
@@ -0,0 +1,27 @@
+import styles from "./product.module.css"
+
+export default function Product (){
+                            
+    return(
+        <div className={styles.card}>
+            	<header className={styles.saleInfo}>
+                    <p>Sale</p>
+                    <p>New</p>
+                </header>
+                <div className={styles.imageWrapper}>
+                    <img src="https://www.shopsector.com/uploads/productgalleryfile/images/1200x1200/sportni-obuvki-puma-voltaic-evo-379601-02-1.jpg" alt="Product image" />
+                </div>
+                <div className={styles.productInfo}>
+                    <div className={styles.productCredentials}>
+                        <p className={styles.name}>Puma</p>
+                        <p className={styles.model}>Voltaic Evo</p>
+                    </div>
+                    <div className={styles.priceDiv}>
+                        <p className={styles.newPrice}>$151.99</p>
+                        <p className={styles.oldPrice}>$189.99</p>
+                    </div>
+                    <p className={styles.freeShipping}>Free shipping</p>
+                </div>
+        </div>
+    );
+}
