60bc439fadfe8e84c1ea3bad4526d007985bc851
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-26 21:52:24 +0300

    Update Product.jsx

--- client/src/components/main/Product/Product.jsx
+++ client/src/components/main/Product/Product.jsx
@@ -1,45 +1,40 @@
 import styles from "./product.module.css"
 
-export default function Product (){
+export default function Product ({
+    data
+}){
                             
     return(
         <div className={styles.card}>
             	<header className={styles.saleInfo}>
-                    <p>Sale</p>
-                    <p>New</p>
+                    {data.isOnSale ? <p className={styles.onSaleFlag}>Sale</p> : null}
+                    {data.isNew ? <p className={styles.newProductFlag}>New</p> : null}
                 </header>
                 <div className={styles.imageWrapper}>
-                    <img src="https://www.shopsector.com/uploads/productgalleryfile/images/1200x1200/sportni-obuvki-puma-voltaic-evo-379601-02-1.jpg" alt="Product image" />
+                    <img src={data.imageUrl} alt="Product image" />
                 </div>
                 <div className={styles.productInfo}>
                     <div className={styles.productCredentials}>
-                        <p className={styles.name}>Puma</p>
-                        <p className={styles.model}>Voltaic Evo</p>
+                        <p className={styles.name}>{data.productName}</p>
+                        <p className={styles.model}>{data.productModel}</p>
                     </div>
                     <div className={styles.priceDiv}>
-                        <p className={styles.oldPrice}>$189.99</p>
-                        <p className={styles.newPrice}>$151.99</p>
+                        <p className={styles.oldPrice}>{ data.isOnSale ? `$${data.oldPrice}` : null }</p>
+                        <p className={styles.price} style={{ color: data.isOnSale ? 'red' : 'black' }}>${data.price}</p>
                     </div>
                     <p className={styles.colorText}>Available colors:</p>
                     <div className={styles.colorsDiv}>
-                        <span className={styles.color} style={{backgroundColor: `red`}}></span>
-                        <span className={styles.color} style={{backgroundColor: `black`}}></span>
-                        <span className={styles.color} style={{backgroundColor: `green`}}></span>
+                        {data['colors'].map(color => (
+                            <span key={color} className={styles.color} style={{backgroundColor: color}}></span>
+                        ))}
                     </div>
                     <p className={styles.sizeText}>Available sizes:</p>
                     <div className={styles.sizesDiv}>
-                        <span className={styles.size}>40</span>
-                        <span className={styles.size}>41</span>
-                        <span className={styles.size}>42</span>
-                        <span className={styles.size}>43</span>
-                        <span className={styles.size}>44</span>
-                        <span className={styles.size}>45</span>
-                        <span className={styles.size}>46</span>
-                        <span className={styles.size}>47</span>
-                        <span className={styles.size}>48</span>
-                        <span className={styles.size}>49</span>
+                        {data['sizes'].map(size => (
+                            <span key={size} className={styles.size}>{size}</span>
+                        ))}
                     </div>
-                    <p className={styles.freeShipping}>Free shipping</p>
+                    {data.price > 100 ? <p className={styles.freeShipping}>Free shipping</p> : null}
                 </div>
         </div>
     );
