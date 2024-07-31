f0a59803e9ed647e102465e27eb1d60cef4a69bd
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-31 22:52:50 +0300

    Create ImageModal.jsx

---
+++ client/src/components/main/ImageModal/ImageModal.jsx
@@ -0,0 +1,40 @@
+import styles from './imageModal.module.css'
+
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+export default function ImageModal({ 
+    onClose,
+    setSelected,
+    imageArray,
+    image, 
+}) {
+
+    let index = imageArray.indexOf(image);
+
+    const previousImage = () => {
+        if(index == 0) {
+            index = 3
+        }
+        setSelected(imageArray[--index]);
+    }
+    const nextImage = () => {
+        if(index == 3){
+            index = 0
+        }
+        setSelected(imageArray[++index]);
+    }
+    return (
+        <div className={styles.modalOverlay} onClick={onClose}>
+            <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+                <div className={styles.iconWrapper} onClick={() => previousImage()}>
+                    <ArrowBackIosIcon className={styles.icon} />
+                </div>
+                <img src={image} alt="Large view" className={styles.modalImage} />
+                <div className={styles.iconWrapper}  onClick={() => nextImage()}>
+                    <ArrowForwardIosIcon />
+                </div>
+            </div>
+        </div>
+    );
+}
