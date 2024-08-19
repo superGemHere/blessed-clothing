5cedc1170be60fedc44f7824a623cc6112db90e8
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-19 23:16:00 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const { constants } = require('./constants');
+const cors = require('cors');
+const mongoose = require('mongoose')
 
 const routes = require('./routes');
+const { auth } = require('./middlewares/authMiddleware')
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/footGear')
+    .then(() => console.log('Database connected successfuly.'))
+    .catch((err) => console.log(err));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(auth);
+
+// app.use((req, res, next) => {
+
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', '*');
+//     res.setHeader('Access-Control-Allow-Headers', '*');
+
+
+//     next();
+// })
 
 app.get('/', (req, res) => {
     res.send('Hello World.')
