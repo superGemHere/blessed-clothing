8d355b07fb58b9cd2dc37aae3d70a4aeb6c03268
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-19 23:15:26 +0300

    Add cookie options

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,62 @@
 const router = require('express').Router();
 
+const userManager = require('../managers/userManager')
 
+router.post('/register', async (req, res) => {
+    try{
+        // console.log(req.body)
+        const result = await userManager.register(req.body);
 
+        res.cookie('accessToken', result.accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+            sameSite: 'Strict', // Controls when cookies are sent with cross-site requests
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+        res.cookie('userInfo', {email: result.email, id: result._id}, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+            sameSite: 'Strict', // Controls when cookies are sent with cross-site requests
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+
+        res.json(result);
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        });
+    }
+})
+
+router.post('/login', async(req, res) => {
+    try{
+        const result = await userManager.login(req.body);
+
+        res.cookie('accessToken', result.accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+            sameSite: 'Strict', // Controls when cookies are sent with cross-site requests
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+        res.cookie('userInfo', {email: result.email, id: result._id}, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+            sameSite: 'Strict', // Controls when cookies are sent with cross-site requests
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+    
+        res.json(result);
+
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        })
+    }
+})
+
+router.get('/logout', async(req, res) => {
+    // TODO: INVALIDATE TOKEN
+    res.end();
+})
 
 module.exports = router;
