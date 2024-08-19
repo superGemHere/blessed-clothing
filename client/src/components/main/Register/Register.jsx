482b69f4c2e215e3a2cc149e0ba300bfa2921337
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-19 22:49:21 +0300

    Update Register.jsx

--- client/src/components/main/Register/Register.jsx
+++ client/src/components/main/Register/Register.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './register.module.css';
 import { Link } from 'react-router-dom';
 
+import { register } from '../../../api/authenticationApi';
+
 export default function Login (){
     const [isHovered, setIsHovered] = useState(false);
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ rePass, setRePass ] = useState('');
+    
+    // useEffect(() => {
+    //     console.log(email)
+    // }, [email])
+
+    const submitHandler = async (e) =>{
+        e.preventDefault();
+
+        const user = await register(email, password, rePass);
+
+        console.log(user);
+    }
                             
     return(
         <div className={styles.container}>
-            <form action="#" className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} method="">
+            <form onSubmit={submitHandler} className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} >
                 <h1>Register</h1>
                 <label htmlFor="email" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Email</label>
-                <input type="text" name="email" className={styles.registerInput} id={styles.email} placeholder='example@gmail.com' />
+                <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} className={styles.registerInput} id={styles.email} placeholder='example@gmail.com' />
                 <label htmlFor="password" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Password</label>
-                <input type="password" name="password" className={styles.registerInput} id={styles.password} placeholder='*****' />
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} className={styles.registerInput} id={styles.password} placeholder='*****' />
                 <label htmlFor="repeatPassword" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Repeat Password</label>
-                <input type="password" name="repeatPassword" className={styles.registerInput} id={styles.password} placeholder='*****' />
+                <input type="password" name="repeatPassword" onChange={(e) => setRePass(e.target.value)}className={styles.registerInput} id={styles.password} placeholder='*****' />
                 <input type='submit' value={"Register"}  
                     onClick={() => setIsHovered(true)}/>
                      <p>Already have an account? Then click<Link className={styles.navigateLink} to={'/users/login'}>here</Link>.</p>
