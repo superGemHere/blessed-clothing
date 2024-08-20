ac3b01ae54558faaa3e239cb07c0e2b1e1342c5d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-20 23:26:19 +0300

    add getAuthResult

--- server/managers/userManager.js
+++ server/managers/userManager.js
@@ -1,55 +1,57 @@
-const bcrypt = require('bcrypt');
-const jwt = require('../lib/jwt');
-const {constants} = require('../constants.js')
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
+const { constants } = require("../constants.js");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-exports.register = async (userData) => {
-    const user = await User.exists({
-        email: userData.email,
-    });
+exports.register = async userData => {
+  const user = await User.exists({
+    email: userData.email
+  });
 
-    if(user){
-        throw new Error("This email is already used.")
-    }
-
-    const createdUser = await  User.create(userData);
-
-    const result = getAuthResult(createdUser);
-    
-    return result;
-}
-exports.login = async ({email, password}) => {
-  const user = await User.findOne({email});
-
-  if(!user){
-    throw new Error('Invalid email or password')
+  if (user) {
+    throw new Error("This email is already used.");
   }
 
-  const isValid = await bcrypt.compare(password, user.password);
+  const createdUser = await User.create(userData);
 
-  if(!isValid){
-    throw new Error('Invalid email or password')
-  }
-  
-  const result = getAuthResult(user);
+  const result = getAuthResult(createdUser);
 
   return result;
+};
 
-}
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email });
 
-const getAuthResult = async (user) => {
-    const payload = {
-        _id: user._id,
-        email: user.email,
-      }
-
-      const token = await jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: '2d' })
-
-      const result = {
-        _id: user._id,
-        email: user.email,
-        accessToken: token,
+    if (!user) {
+        throw new Error("Invalid email or password");
     }
-      return result;
-}
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error("Invalid email or password");
+    }
+
+    const result = await getAuthResult(user);
+
+    return result;
+};
+
+const getAuthResult = async user => {
+  const payload = {
+    _id: user._id,
+    email: user.email
+  };
+
+  const token = await jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: "2d"
+  });
+
+  const result = {
+    _id: user._id,
+    email: user.email,
+    accessToken: token
+  };
+  return result;
+};
