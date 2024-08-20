d3311afa1445c7730a1d4b2c4dfe5e5dc689b85a
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-20 16:03:21 +0300

    Update registerHandler

--- client/src/components/main/Register/Register.jsx
+++ client/src/components/main/Register/Register.jsx
@@ -3,28 +3,19 @@ import styles from './register.module.css';
 import { Link } from 'react-router-dom';
 
 import { register } from '../../../api/authenticationApi';
+import { useAuth } from '../../../Context/authContext';
 
 export default function Login (){
+    const { registerSubmitHandler } = useAuth();
     const [isHovered, setIsHovered] = useState(false);
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ rePass, setRePass ] = useState('');
     
-    // useEffect(() => {
-    //     console.log(email)
-    // }, [email])
-
-    const submitHandler = async (e) =>{
-        e.preventDefault();
-
-        const user = await register(email, password, rePass);
-
-        console.log(user);
-    }
                             
     return(
         <div className={styles.container}>
-            <form onSubmit={submitHandler} className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} >
+            <form onSubmit={(e) => registerSubmitHandler(e, email, password, rePass)} className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} >
                 <h1>Register</h1>
                 <label htmlFor="email" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Email</label>
                 <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} className={styles.registerInput} id={styles.email} placeholder='example@gmail.com' />
