aaab63443dfdab6bbde9f253098625c7264e05b5
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-20 20:45:16 +0300

    ADD submit handler to login

--- client/src/components/main/Login/Login.jsx
+++ client/src/components/main/Login/Login.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import styles from './login.module.css';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../../Context/authContext';
 
 export default function Login (){
+    const {loginSubmitHandler} = useAuth();
     const [isHovered, setIsHovered] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
                             
     return(
         <div className={styles.container}>
-            <form action="#" className={`${styles.loginForm} ${isHovered ? styles.loginFormHovered : ''}`} method="">
+            <form  onSubmit={(e) => loginSubmitHandler(e, email, password)}className={`${styles.loginForm} ${isHovered ? styles.loginFormHovered : ''}`} >
                 <h1>Log-in</h1>
                 <label htmlFor="email" className={isHovered ? styles.loginLabelsHovered : styles.loginLabels} >Email</label>
-                <input type="text" name="email" className={styles.loginInput} id={styles.email} placeholder='example@gmail.com' />
+                <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} className={styles.loginInput} id={styles.email} placeholder='example@gmail.com' />
                 <label htmlFor="password" className={isHovered ? styles.loginLabelsHovered : styles.loginLabels} >Password</label>
-                <input type="password" name="password" className={styles.loginInput} id={styles.password} placeholder='*****' />
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} className={styles.loginInput} id={styles.password} placeholder='*****' />
                 <input type='submit' value={"Login"}  
                     onClick={() => setIsHovered(true)}/>
                     <p>Don't have an account? Create your account<Link className={styles.navigateLink} to={'/users/register'}>here</Link>.</p>
