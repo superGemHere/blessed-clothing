35c2914a77aa0206177964a9e0b102a2bb2f672c
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-21 16:21:32 +0300

    set error message

--- client/src/components/main/Register/Register.jsx
+++ client/src/components/main/Register/Register.jsx
@@ -11,18 +11,24 @@ export default function Login (){
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ rePass, setRePass ] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     
                             
     return(
         <div className={styles.container}>
-            <form onSubmit={(e) => registerSubmitHandler(e, email, password, rePass)} className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} >
+            {/* <form onSubmit={(e) => registerSubmitHandler(e, email, password, rePass, setErrorMessage)} className={`${styles.registerForm} ${isHovered ? styles.registerFormHovered : ''}`} > */}
+            <form onSubmit={(e) => registerSubmitHandler(e, email, password, rePass, setErrorMessage)} className={styles.registerForm} >
                 <h1>Register</h1>
-                <label htmlFor="email" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Email</label>
+                {/* <label htmlFor="email" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Email</label> */}
+                <label htmlFor="email" className={styles.registerLabels} >Email</label>
                 <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} className={styles.registerInput} id={styles.email} placeholder='example@gmail.com' />
-                <label htmlFor="password" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Password</label>
+                {/* <label htmlFor="password" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Password</label> */}
+                <label htmlFor="password" className={styles.registerLabels} >Password</label>
                 <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} className={styles.registerInput} id={styles.password} placeholder='*****' />
-                <label htmlFor="repeatPassword" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Repeat Password</label>
+                {/* <label htmlFor="repeatPassword" className={isHovered ? styles.registerLabelsHovered : styles.registerLabels} >Repeat Password</label> */}
+                <label htmlFor="repeatPassword" className={styles.registerLabels} >Repeat Password</label>
                 <input type="password" name="repeatPassword" onChange={(e) => setRePass(e.target.value)}className={styles.registerInput} id={styles.password} placeholder='*****' />
+                <p className={styles.errorMessage}>{errorMessage}</p>
                 <input type='submit' value={"Register"}  
                     onClick={() => setIsHovered(true)}/>
                      <p>Already have an account? Then click<Link className={styles.navigateLink} to={'/users/login'}>here</Link>.</p>
