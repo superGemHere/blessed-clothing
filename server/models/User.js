2a58201b99fb908527d5a6a22d73852caa1646b8
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-21 16:22:56 +0300

    Add constraints

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,33 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email."]
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [8, "The password must be between 8-16 characters length."],
+    maxLength: [16, "The password must be between 8-16 characters length."]
+  }
+});
 
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+userSchema.virtual("repeatPassword").set(function(value) {
+  if (this.password !== value) {
+    throw new Error("Provided password's do not match.");
+  }
+});
 
-})
+userSchema.pre("save", async function() {
+  const hash = await bcrypt.hash(this.password, 10);
 
-userSchema.pre('save', async function () {
+  this.password = hash;
+});
 
-    const hash = await bcrypt.hash(this.password, 10);
-
-    this.password = hash;
-
-})
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
