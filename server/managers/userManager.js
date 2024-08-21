ae187ba2756f609d4f8d80a6ca52cbb466c7c2e1
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-21 16:22:37 +0300

    modify register service

--- server/managers/userManager.js
+++ server/managers/userManager.js
@@ -1,23 +1,34 @@
 const bcrypt = require("bcrypt");
 const jwt = require("../lib/jwt");
+const extractErrorMessage = require('../utils/errorMsgHandler.js')
 const { constants } = require("../constants.js");
 
 const User = require("../models/User");
 
-exports.register = async userData => {
-  const user = await User.exists({
-    email: userData.email
-  });
+exports.register = async (userData) => {
+  try {
+    // Check if the email is already in use
+    // const userExists = await User.exists({
+    //   email: userData.email
+    // });
 
-  if (user) {
-    throw new Error("This email is already used.");
+    // if (userExists) {
+    //   throw new Error("This email is already used.");
+    // }
+
+    // Create the new user
+    const createdUser = await User.create(userData);
+
+    // Generate authentication result (e.g., token, user data, etc.)
+    const result = getAuthResult(createdUser);
+
+    return result;
+
+  } catch (err) {
+    // Handle specific error cases
+    const message = extractErrorMessage(err)
+    throw new Error(message)
   }
-
-  const createdUser = await User.create(userData);
-
-  const result = getAuthResult(createdUser);
-
-  return result;
 };
 
 exports.login = async ({ email, password }) => {
