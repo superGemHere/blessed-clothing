7f9983a353f83c7b8488baf47b6d1e44936524d2
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-29 13:00:28 +0300

    Create AdminPanel.jsx

---
+++ client/src/components/main/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,10 @@
+import styles from './adminPanel.styles.css';
+
+export default function AdminPanel (){
+                            
+    return(
+        <div className={styles.container}>
+            				
+        </div>
+    );
+}
