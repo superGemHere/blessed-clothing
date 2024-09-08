b072a18bd7dec7026700caacf7b3336cb93cb9c2
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-09-08 19:49:15 +0300

    chore: add auth middleware

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,22 +1,19 @@
 const router = require('express').Router();
-
 const productManager = require('../managers/productManager');
+const {auth} = require('../middlewares/authMiddleware'); // Import your middleware
 
-router.post('/create', async (req, res) => {
-   try {
-       const response = await productManager.create(req.body);
-
-       res.status(201).json({
-           message: 'Product created successfully.'
-       });
-
-   } catch (err) {
-    res.status(400).json({ 
-        message: err.message 
-    });
-   }
-
-
+// Apply the verifyToken middleware to the product creation route
+router.post('/create', auth, async (req, res) => {
+    try {
+        const response = await productManager.create(req.body);
+        res.status(201).json({
+            message: 'Product created successfully.'
+        });
+    } catch (err) {
+        res.status(400).json({ 
+            message: err.message 
+        });
+    }
 });
 
 module.exports = router;
