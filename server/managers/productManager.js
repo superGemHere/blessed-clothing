36a0745ffef9df3e121d401bbeecd3105607e1a5
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-09-17 18:47:36 +0300

    chore: add gelAll, getById and getPaginatedProduct managers

--- server/managers/productManager.js
+++ server/managers/productManager.js
@@ -1,3 +1,27 @@
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
-exports.create = (productData) => Product.create(productData);
+exports.getAll = () => Product.find();
+exports.getById = productId => Product.findById(productId);
+exports.create = productData => Product.create(productData);
+
+exports.getPaginatedProducts = async (page = 1, limit = 10) => {
+  try {
+    // Calculate the number of documents to skip
+    const skip = (page - 1) * limit;
+
+    // Fetch the paginated products
+    const products = await Product.find().skip(skip).limit(limit);
+
+    // Get the total count of documents in the collection
+    const totalCount = await Product.countDocuments();
+
+    return {
+      products,
+      totalCount,
+      currentPage: page,
+      totalPages: Math.ceil(totalCount / limit)
+    };
+  } catch (error) {
+    throw new Error("Error fetching paginated products: " + error.message);
+  }
+};
